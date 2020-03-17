ff3fc2fedc06baea9740ee1c2d2f50686af91d82
hari <hari2041993@gmail.com>
2020-03-17 22:57:47 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import News_component from './components/news_component/news_component'
+import Nav_component from './components/nav_component/nav_component'
 
 function App() {
   return (
-    <div className="App">
+    <div className="w-100">
+        <Nav_component />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <News_component />
       </header>
     </div>
   );
